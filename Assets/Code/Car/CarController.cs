f5f5e9259f255bb2f3a5371deca57ee08de2f189
echonic <echonic@gmail.com>
2015-12-19 21:26:28 -0600

    basic driving on terrain

---
+++ Assets/Code/Car/CarController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+    public WheelCollider FrontLeftWheel;
+    public WheelCollider FrontRightWheel;
+    public WheelCollider BackLeftWheel;
+    public WheelCollider BackRightWheel;
+
+    public Transform FrontLeftWheelModel;
+    public Transform FrontRightWheelModel;
+
+    public float EngineTorque = 5f;
+    public float SteeringAngle = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            BackLeftWheel.motorTorque = EngineTorque;
+            BackRightWheel.motorTorque = EngineTorque;
+//            GetComponent<ConstantForce>().relativeForce = Vector3.forward * 120 * 20;
+        }
+        else
+        {
+            BackLeftWheel.motorTorque = 0f;
+            BackRightWheel.motorTorque = 0f;
+            //            GetComponent<ConstantForce>().relativeForce = Vector3.zero;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            FrontLeftWheel.steerAngle = -SteeringAngle;
+            FrontRightWheel.steerAngle = -SteeringAngle;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            FrontLeftWheel.steerAngle = SteeringAngle;
+            FrontRightWheel.steerAngle = SteeringAngle;
+        }
+        else
+        {
+            FrontLeftWheel.steerAngle = 0;
+            FrontRightWheel.steerAngle = 0;
+        }
+
+        UpdateTireMeshes();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            FixCar();
+        }
+    }
+
+    void UpdateTireMeshes()
+    {
+        FrontLeftWheelModel.localRotation = Quaternion.Euler(
+            FrontLeftWheelModel.localRotation.eulerAngles.x,
+            FrontLeftWheel.steerAngle,
+            FrontLeftWheelModel.localRotation.eulerAngles.z);
+        FrontRightWheelModel.localRotation = Quaternion.Euler(
+            FrontRightWheelModel.localRotation.eulerAngles.x,
+            FrontLeftWheel.steerAngle,
+            FrontRightWheelModel.localRotation.eulerAngles.z);
+    }
+
+    void FixCar()
+    {
+        transform.localRotation = Quaternion.Euler(0f, transform.localRotation.eulerAngles.y, 0f);
+    }
+}
